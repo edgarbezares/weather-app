9902135b4436cfe7c6cfd2e8f0af96964422b781
Edgar Bezares <a19216288@alumnos.uady.mx>
2024-06-19 17:46:09 -0600

    feat(home): display current day and date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 import axios from "axios";
 import useSWR from 'swr';
 import { WeatherData } from "@/interfaces/weather";
+import { format,parseISO } from "date-fns";
 const fetcher = (url: string) => axios.get(url).then(res => res.data);
 
 export default function Home() {
@@ -10,15 +11,27 @@ export default function Home() {
 
   const { data, error } = useSWR<WeatherData>(urlAPI, fetcher);
 
-  console.log("data",data);
+  const infoData=data?.list[0];
+
   
   if (!data) return 'Loading...';
 
   if (error) return 'An error has occurred';
 
   return (
-    <div className="flex-flex-col gap-4 bg-gray-100 min-h-screen">
+    <div className="flex-flex-col gap-4 bg-[rgb(43,74,99)] min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        <section>
+          <div>
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p className="text-white">{format(parseISO(infoData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className=" text-white">- {format(parseISO(infoData?.dt_txt ?? ''), 'dd.MM.yy')}</p>
+            </h2>
+            <div></div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
